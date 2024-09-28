c6382d5d1936f227c9bb5adf91fa9a75c5962fcc
sulaiman001221 <sulaimanndlovu@gmail.com>
2024-09-29 01:08:03 +0200

    made the tests DRY

--- spec/memory_game_spec.js
+++ spec/memory_game_spec.js
@@ -11,7 +11,25 @@ describe("Memory Game", () => {
     restartButton,
     startGameMessage,
     memoryGame,
-    dom;
+    mockCards,
+    mockCard,
+    matchCard1,
+    matchCard2,
+    mockCardsArray;
+
+  const flipAllMatchingPairs = () => {
+    mockCardsArray.forEach((card) => {
+      card.click();
+      const matchingCard = mockCardsArray.find(
+        (matchCard) =>
+          matchCard.dataset.symbol === card.dataset.symbol && matchCard !== card
+      );
+      if (matchingCard) {
+        matchingCard.click();
+      }
+    });
+    jasmine.clock().tick(500);
+  };
 
   beforeEach(() => {
     const setup = setupDom();
@@ -23,68 +41,75 @@ describe("Memory Game", () => {
     container = setup.container;
     restartButton = setup.restartButton;
     startGameMessage = setup.startGameMessage;
-    dom = setup.dom;
     memoryGame = new MemoryGame(document);
+    mockCards = gameBoard.querySelectorAll(".card");
+    mockCard = mockCards[0];
+    mockCardsArray = Array.from(gameBoard.querySelectorAll(".card"));
+    matchCard1 = mockCardsArray[0];
+    matchCard2 = mockCardsArray.find(
+      (card) =>
+        card.dataset.symbol === matchCard1.dataset.symbol && card !== matchCard1
+    );
     jasmine.clock().install();
 
     spyOn(memoryGame, "createBoard").and.callThrough();
-    spyOn(memoryGame, "shuffle").and.callThrough();
+    spyOn(memoryGame, "disableCards").and.callThrough();
     memoryGame.startGame();
+    spyOn(memoryGame, "shuffle").and.callThrough();
+    spyOn(memoryGame, "enableCards").and.callThrough();
+    spyOn(memoryGame, "flipCard").and.callThrough();
+    spyOn(memoryGame, "checkMatch").and.callThrough();
+    spyOn(memoryGame, "displayRestartButton").and.callThrough();
+    spyOn(memoryGame, "displayWinMessage").and.callThrough();
+    spyOn(memoryGame, "startGame").and.callThrough();
   });
 
   afterEach(() => {
     document.body.innerHTML = "";
     jasmine.clock().uninstall();
+    restartButton.click();
   });
 
   describe("Board Initialization", () => {
     it("should create a board with a duplicate of available symbols", () => {
-      expect(memoryGame.createBoard).toHaveBeenCalled();
-      const mockCards = gameBoard.querySelectorAll(".card");
+      expect(memoryGame.createBoard).toHaveBeenCalledTimes(1);
       expect(mockCards.length).toBe(memoryGame.symbols.length * 2);
     });
 
-    it("should create a board with disabled cards", () => {
-      expect(memoryGame.createBoard).toHaveBeenCalled();
-      const mockCards = gameBoard.querySelectorAll(".card");
-      mockCards.forEach((card) => {
-        expect(card.classList.contains("disabled")).toBe(true);
-      });
+    it("should shuffle the cards when the board is created", () => {
+      memoryGame.createBoard();
+      expect(memoryGame.shuffle).toHaveBeenCalledOnceWith(
+        jasmine.arrayContaining([...memoryGame.symbols, ...memoryGame.symbols])
+      );
     });
 
-    it("should shuffle the cards when the game has been started", () => {
-      const initialOrder = [...memoryGame.symbols, ...memoryGame.symbols];
-      const newCards = gameBoard.querySelectorAll(".card");
-      const newOrder = Array.from(newCards).map((card) => card.dataset.symbol);
-      expect(newOrder).not.toEqual(initialOrder);
+    it("should disable all cards after creation", () => {
+      expect(memoryGame.disableCards).toHaveBeenCalledTimes(1);
+      mockCards.forEach((card) => {
+        expect(card.classList.contains("disabled")).toBeTrue();
+      });
     });
   });
 
   describe("Start Game Event", () => {
     it("should start the game and initially hide the restart button when the start button is clicked", () => {
-      spyOn(memoryGame, "enableCards").and.callThrough();
       startButton.click();
-
       expect(startGameMessage.style.display).toBe("none");
       expect(container.classList.contains("fully-bright-container")).toBe(true);
-      expect(memoryGame.enableCards).toHaveBeenCalled();
+      expect(memoryGame.enableCards).toHaveBeenCalledTimes(1);
       expect(window.getComputedStyle(restartButton).display).toBe("none");
     });
   });
 
   describe("Card Interaction", () => {
     it("should flip a card when clicked", () => {
-      const mockCard = gameBoard.querySelector(".card");
-      spyOn(memoryGame, "flipCard").and.callThrough();
       mockCard.click();
-      expect(memoryGame.flipCard).toHaveBeenCalledWith(mockCard);
+      expect(memoryGame.flipCard).toHaveBeenCalledOnceWith(mockCard);
       expect(mockCard.classList.contains("hidden")).toBe(false);
       expect(mockCard.textContent).toBe(mockCard.dataset.symbol);
     });
 
     it("should not allow a flipped card to be re-flipped", () => {
-      const mockCard = gameBoard.querySelector(".card");
-      spyOn(memoryGame, "flipCard").and.callThrough();
       mockCard.click();
       mockCard.click();
       expect(memoryGame.flipCard).toHaveBeenCalledOnceWith(mockCard);
@@ -92,10 +117,9 @@ describe("Memory Game", () => {
     });
 
     it("should not allow flipping more than two cards at the same time", () => {
-      const mockCard = gameBoard.querySelectorAll(".card");
-      const card1 = mockCard[0];
-      const card2 = mockCard[1];
-      const card3 = mockCard[2];
+      const card1 = mockCards[1];
+      const card2 = mockCards[2];
+      const card3 = mockCards[4];
 
       card1.click();
       card2.click();
@@ -107,89 +131,66 @@ describe("Memory Game", () => {
     });
 
     it("should hide the opened cards if their symbols are different", () => {
-      const mockCardsArray = Array.from(gameBoard.querySelectorAll(".card"));
-      const card1 = mockCardsArray[0];
-      const card2 = mockCardsArray.find(
-        (card) => card.dataset.symbol !== card1.dataset.symbol && card !== card1
+      const randomCard = mockCards[3];
+      const differentCard = mockCardsArray.find(
+        (card) => card.dataset.symbol !== randomCard.dataset.symbol
       );
-
-      spyOn(memoryGame, "checkMatch").and.callThrough();
-      card1.click();
-      card2.click();
-
-      expect(memoryGame.checkMatch).toHaveBeenCalled();
+      randomCard.click();
+      differentCard.click();
+      expect(memoryGame.checkMatch).toHaveBeenCalledTimes(1);
       jasmine.clock().tick(1000);
-      expect(card1.classList.contains("hidden")).toBe(true);
-      expect(card2.classList.contains("hidden")).toBe(true);
+      expect(matchCard1.classList.contains("hidden")).toBe(true);
+      expect(matchCard2.classList.contains("hidden")).toBe(true);
     });
 
     it("should display the restart button when the first card is flipped", () => {
-      const mockCards = gameBoard.querySelectorAll(".card");
-      const card = mockCards[0];
-      expect(restartButton.style.display).toBe("");
-      spyOn(memoryGame, "displayRestartButton").and.callThrough();
-      card.click();
-      expect(memoryGame.displayRestartButton).toHaveBeenCalled();
+      expect(window.getComputedStyle(restartButton).display).toBe("none");
+      mockCard.click();
+      expect(memoryGame.displayRestartButton).toHaveBeenCalledTimes(1);
       expect(restartButton.style.display).toBe("block");
     });
 
     it("should match the cards when cards of similar symbols are opened", () => {
-      const mockCardsArray = Array.from(gameBoard.querySelectorAll(".card"));
-      const card1 = mockCardsArray[0];
-      const card2 = mockCardsArray.find(
-        (card) => card.dataset.symbol === card1.dataset.symbol && card !== card1
-      );
-      spyOn(memoryGame, "checkMatch").and.callThrough();
-      card1.click();
-      card2.click();
+      matchCard1.click();
+      matchCard2.click();
 
-      expect(memoryGame.checkMatch).toHaveBeenCalled();
-      expect(card1.classList.contains("matched")).toBe(true);
-      expect(card2.classList.contains("matched")).toBe(true);
+      expect(memoryGame.checkMatch).toHaveBeenCalledTimes(1);
+      expect(matchCard1.classList.contains("matched")).toBe(true);
+      expect(matchCard2.classList.contains("matched")).toBe(true);
     });
 
     it("should not flip matched cards", () => {
-      const mockCardsArray = Array.from(gameBoard.querySelectorAll(".card"));
-      const card1 = mockCardsArray[0];
-      const card2 = mockCardsArray.find(
-        (card) => card.dataset.symbol === card1.dataset.symbol && card !== card1
-      );
-      card1.click();
-      card2.click();
-      card1.click();
-
-      expect(card1.classList.contains("hidden")).toBe(false);
+      matchCard1.click();
+      matchCard2.click();
+      matchCard1.click();
+      expect(matchCard1.classList.contains("hidden")).toBe(false);
     });
   });
 
   describe("Restart Game Event", () => {
     it("should restart the game when the restart button is clicked", () => {
-      spyOn(memoryGame, "startGame").and.callThrough();
       restartButton.click();
-      expect(memoryGame.startGame).toHaveBeenCalled();
+      expect(memoryGame.startGame).toHaveBeenCalledTimes(1);
       expect(startGameMessage.style.display).toBe("block");
       expect(restartButton.style.display).toBe("none");
     });
 
-    it("should land on the start game page when the play again button is clicked", () => {
-      const mockCardsArray = Array.from(gameBoard.querySelectorAll(".card"));
-      mockCardsArray.forEach((card) => {
-        const card1 = card;
-        card1.click();
-        const card2 = mockCardsArray.find(
-          (matchCard) =>
-            matchCard.dataset.symbol === card1.dataset.symbol &&
-            matchCard !== card1
-        );
-        card2.click();
+    it("should reset all matched cards when restart button is clicked", () => {
+      matchCard1.click();
+      matchCard2.click();
+      restartButton.click();
+      memoryGame.cards.forEach((card) => {
+        expect(card.classList.contains("matched")).toBe(false);
       });
-      jasmine.clock().tick(500);
+    });
+
+    it("should land on the start game page when the play again button is clicked", () => {
+      flipAllMatchingPairs();
       expect(window.getComputedStyle(winPopupMessage).display).toBe("block");
 
       const playAgainButton = document.querySelector("#play-again-button");
-      spyOn(memoryGame, "startGame").and.callThrough();
       playAgainButton.click();
-      expect(memoryGame.startGame).toHaveBeenCalled();
+      expect(memoryGame.startGame).toHaveBeenCalledTimes(1);
       expect(winPopupMessage.style.display).toBe("none");
       expect(startGameMessage.style.display).toBe("block");
     });
@@ -197,20 +198,8 @@ describe("Memory Game", () => {
 
   describe("Win Scenario", () => {
     it("should display a win message when all the cards have been matched", () => {
-      const mockCardsArray = Array.from(gameBoard.querySelectorAll(".card"));
-      spyOn(memoryGame, "displayWinMessage").and.callThrough();
-      mockCardsArray.forEach((card) => {
-        const card1 = card;
-        card1.click();
-        const card2 = mockCardsArray.find(
-          (matchCard) =>
-            matchCard.dataset.symbol === card1.dataset.symbol &&
-            matchCard !== card1
-        );
-        card2.click();
-      });
-      jasmine.clock().tick(500);
-      expect(memoryGame.displayWinMessage).toHaveBeenCalled();
+      flipAllMatchingPairs();
+      expect(memoryGame.displayWinMessage).toHaveBeenCalledTimes(8);
       expect(winPopupMessage.style.display).toBe("block");
       expect(container.classList.contains("fully-bright-container")).toBe(
         false
