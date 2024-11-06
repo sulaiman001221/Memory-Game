3c4eb21a349a0595004eee0ee3cc38c406898b58
sulaiman001221 <sulaimanndlovu@gmail.com>
2024-11-06 23:45:36 +0200

    added card clicks

--- src/memory_game.js
+++ src/memory_game.js
@@ -22,6 +22,7 @@ class MemoryGame {
     this.firstCard = null;
     this.secondCard = null;
     this.matchedPairs = 0;
+    this.flipCount = 0;
 
     this.setupDom();
   }
@@ -39,7 +40,8 @@ class MemoryGame {
       this.document.querySelector("#grid-button-3x4");
     this.fourByFourGridButton = this.document.querySelector("#grid-button-4x4");
     this.timerDisplay = this.document.querySelector("#timer");
-    
+    this.flipCountDisplay = document.getElementById("flip-count");
+
     this.fourByFourButtonHandler();
     this.setupEventListeners();
   }
@@ -80,6 +82,16 @@ class MemoryGame {
     this.timerInterval = null;
   }
 
+  incrementFlipCount() {
+    this.flipCount += 1;
+    this.flipCountDisplay.textContent = flipCount;
+  }
+
+  resetFlipCount() {
+    flipCount = 0;
+    this.flipCountDisplay.textContent = flipCount;
+  }
+
   createBoard(cardsNum) {
     if (!this.gameBoard) return;
     this.cards = [];
@@ -107,6 +119,7 @@ class MemoryGame {
     if (this.isCardClickable(card)) {
       this.flipCard(card);
       this.displayRestartButton();
+      this.incrementFlipCount();
     }
   }
 
@@ -127,6 +140,7 @@ class MemoryGame {
   }
 
   restartGameHandler() {
+    this.resetFlipCount;
     this.stopTimer();
     this.timerDisplay.textContent = "00:00";
 
@@ -137,6 +151,7 @@ class MemoryGame {
   }
 
   playAgainHandler() {
+    this.resetFlipCount();
     this.stopTimer();
     this.timerDisplay.textContent = "00:00";
 
